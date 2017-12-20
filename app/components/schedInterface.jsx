933590ada21ad30727b8066617927fdb4d689822
Glitch (honey-bean) <none>
2017-12-20 06:36:58 +0000

    Start schedule interface form and table

--- app/components/schedInterface.jsx
+++ app/components/schedInterface.jsx
@@ -9,14 +9,40 @@ class ScheduleInterface extends React.Component{
     //this sends x as a callback to the parent in teammates.jsx.. Similar to menu
     this.props.selector(x);
   }
-  interface(){
+  week(){
     return(
     <div>
-      <h4 className="inline">
-        Scheduler
-        </h4>
-        
-      </div>)
+         <form method="POST" action="saveSchedule" target="hiddenFrame">
+        <table>
+          <tr>
+    <th>Sunday</th>
+    <th>Monday</th>
+    <th>Tuesday</th>
+    <th>Wednesday</th>
+    <th>Thursday</th>
+    <th>Friday</th>
+    <th>Saturday</th>
+  </tr>
+ 
+  <tr>
+     <td><input name = "sunStart" placeholder="start" className="timeInput"/> <input name = "sunEnd" placeholder="end" className="timeInput"/></td>
+     <td><input name = "monStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+     <td><input name = "tueStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+     <td><input name = "wedStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+     <td><input name = "thuStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+     <td><input name = "friStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+     <td><input name = "satStart" className="timeInput"/> <input name = "sunEnd"  className="timeInput"/></td>
+  </tr> 
+  
+        </table>
+           <hr/>
+     <button className = "saveButton" type = "submit">
+    Save Changes
+    </button>
+             </form>
+       
+          
+   </div>)
   }
   userList(){
     return(
@@ -35,6 +61,10 @@ class ScheduleInterface extends React.Component{
         {this.userList()}
       </div>
         <div className="rightSide">
+          <center>
+            <h2 className="title2">Schedule</h2>
+          </center>
+          {this.week()}
         </div>
       </div>
         )
